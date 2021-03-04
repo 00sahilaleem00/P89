47d185ad0251e9193e82f9c4e6151315f1c3c19c
00sahilaleem00 <00sahilaleem00@gmail.com>
2021-03-04 18:10:56 +0530

    first commit

--- screens/SignUpLoginScreen.js
+++ screens/SignUpLoginScreen.js
@@ -8,6 +8,9 @@ import {
   StyleSheet,
   Header,
   Alert,
+  Modal,
+  ScrollView,
+  KeyboardAvoidingView,
 } from "react-native";
 import db from "../config.js";
 import firebase from "firebase";
@@ -18,22 +21,48 @@ export default class SignUpLoginScreen extends Component {
     this.state = {
       email: "",
       password: "",
+      confirmPassword: "",
+      first_name: "",
+      last_name: "",
+      address: "",
+      mobile_number: "",
+      isModalVisible: false,
     };
   }
 
-  userSignUp = (email, password) => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((response) => {
-        return Alert.alert("User Added Successfully");
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        return Alert.alert(errorMessage);
-      });
-  };
+  userSignUp(email, password, confirmPassword) {
+    if (password != confirmPassword) {
+      Alert.alert("Passwords Must Match!");
+    } else {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          db.collection("users").add({
+            Address: this.state.address,
+            First_Name: this.state.first_name,
+            Last_Name: this.state.last_name,
+            Mobile_Number: this.state.mobile_number,
+            Username: this.state.email,
+          });
+          return Alert.alert("User Added Successfully", "", [
+            {
+              text: "OK",
+              onPress: () => {
+                this.setState({
+                  isModalVisible: false,
+                });
+              },
+            },
+          ]);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          return Alert.alert(errorMessage);
+        });
+    }
+  }
 
   userLogin = (email, password) => {
     firebase
@@ -49,9 +78,109 @@ export default class SignUpLoginScreen extends Component {
       });
   };
 
+  showModal = () => {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={this.state.isModalVisible}
+      >
+        <View style={styles.modalContainer}></View>
+        <ScrollView style={{ width: "80%" }} />
+        <KeyboardAvoidingView style={styles.keyboardAvoidingView}>
+          <Text style={styles.modalTitle}>REGISTRATION</Text>
+          <TextInput
+            style={styles.inputBox}
+            placeholder={"emailID@example.com"}
+            keyboardType="email-address"
+            onChangeText={(text) => {
+              this.setState({
+                email: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              this.setState({
+                password: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              this.setState({
+                confirmPassword: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder={"First Name"}
+            onChangeText={(text) => {
+              this.setState({
+                first_name: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder={"Last Name"}
+            onChangeText={(text) => {
+              this.setState({
+                last_name: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder={"Address"}
+            onChangeText={(text) => {
+              this.setState({
+                address: text,
+              });
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder={"contact number"}
+            keyboardType="numeric"
+            maxLength={10}
+            onChangeText={(text) => {
+              this.setState({
+                mobile_number: text,
+              });
+            }}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              this.setState({ isModalVisible: false });
+            }}
+          >
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              this.userSignUp(
+                this.state.email,
+                this.state.password,
+                this.state.confirmPassword
+              );
+            }}
+          >
+            <Text>REGISTER</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </Modal>
+    );
+  };
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.container}>{this.showModal()}</View>
         <View>
           <TextInput
             style={styles.inputBox}
@@ -87,7 +216,9 @@ export default class SignUpLoginScreen extends Component {
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              this.userSignUp(this.state.email, this.state.password);
+              this.setState({
+                isModalVisible: true,
+              });
             }}
           >
             <Text>Sign Up</Text>
@@ -111,6 +242,15 @@ const styles = StyleSheet.create({
   },
   button: {
     margin: 10,
-    backgroundColor: "red",
+    backgroundColor: "blue",
+  },
+  modalContainer: {
+    margin: 10,
+  },
+  keyboardAvoidingView: {
+    margin: 10,
+  },
+  modalTitle: {
+    backgroundColor: "green",
   },
 });
