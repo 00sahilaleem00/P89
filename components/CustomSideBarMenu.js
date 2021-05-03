1a5a7703db825e7b399675d45b027f568e69c557
unknown <00sahilaleem00@gmail.com>
2021-05-03 16:14:15 +0530

    luftwaffe

--- components/CustomSideBarMenu.js
+++ components/CustomSideBarMenu.js
@@ -1,13 +1,97 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { DrawerItems } from "react-navigation-drawer";
-
+import { Header, Icon, Avatar } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import db from "../config";
 import firebase from "firebase";
 
 export default class CustomSideBarMenu extends Component {
+  state = {
+    image: "#",
+    userID: firebase.auth().currentUser.email,
+    name: "",
+    documentID: "",
+  };
+
+  selectPicture = async () => {
+    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!cancelled) {
+      this.setState({
+        image: uri,
+      });
+      this.uploadImage(uri, this.state.userID);
+    }
+  };
+
+  uploadImage = async (uri, imageName) => {
+    var response = await fetch(uri);
+    var blob = await response.blob();
+    var ref = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
+    return ref.put(blob).then((response) => {
+      this.fetchImage(imageName);
+    });
+  };
+
+  getUserProfile() {
+    db.collection("users")
+      .where("User_ID", "==", this.state.userID)
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.setState({
+            name: doc.data().First_Name + " " + doc.data().Last_name,
+          });
+        });
+      });
+  }
+
+  fetchImage = async (imageName) => {
+    var storageRef = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
+    storageRef.getDownloadURL().then((url) => {
+      try {
+        this.setState({
+          image: url,
+        });
+      } catch (error) {
+        console.log(error.message);
+        this.setState({
+          image: "#",
+        });
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.getUserProfile();
+    this.fetchImage(this.state.userID);
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, marginTop: 50 }}>
+        <Avatar
+          rounded
+          source={{ uri: this.state.image }}
+          size="medium"
+          onPress={() => {
+            this.selectPicture();
+          }}
+          showEditButton
+        />
+        <Text style={{ fontWeight: "bold", fontSize: 20, padding: 5 }}>
+          {this.state.name}
+        </Text>
         <DrawerItems {...this.props} />
         <View
           style={{ flex: 1, justifyContent: "flex-end", paddingBottom: 30 }}
